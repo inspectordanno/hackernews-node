8daee6e22bf823125a6b80d622872e5e2536241e
Dan Spector <danno2423@gmail.com>
2021-05-31 17:22:37 -0400

    updated .eslintrc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const { ApolloServer } = require('apollo-server');
 
-const poop = ['pee', 'poop'];
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => null,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
