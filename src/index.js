10e0efa2362be84a185b4ae0d665d10b19f4b8c6
Dan Spector <danno2423@gmail.com>
2021-06-06 21:57:25 -0400

    Query link(id:ID) working

--- src/index.js
+++ src/index.js
@@ -1,19 +1,76 @@
+//where I left off:
+//Exercise at bottom of https://www.howtographql.com/graphql-js/3-a-simple-mutation/
+//Query link(id: ID) working
+//Mutation updateLink() and deleteLink() not working
+
+const fs = require('fs');
+const path = require('path');
 const { ApolloServer } = require('apollo-server');
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
+let idCount = links.length;
+
+const findLinkById = (id) => links.find((link) => link.id === id);
+const findLinkIndex = (id) => links.findIndex((link) => link.id === id);
 
 const resolvers = {
   Query: {
     info: () => null,
+    feed: () => links,
+    link: (parent, args) => {
+      const foundLink = findLinkById(args.id);
+      if (foundLink) {
+        return foundLink;
+      }
+      return null; //return null if not found (research better error handling)
+    },
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const { id, url, description } = args;
+      const linkIndex = findLinkIndex(id);
+
+      //checks if linkIndex exists
+      if (Math.sign(linkIndex) >= 0) {
+        const link = links[linkIndex];
+        const updates = {
+          ...link,
+          ...(url && { url }),
+          ...(description && { description }),
+        };
+        links[linkIndex] = updates;
+      }
+      return null; //return null if not found
+    },
+    deleteLink: (parent, args) => {
+      const { id } = args;
+      const linkIndex = findLinkIndex(args.id);
+      if (Math.sign(linkIndex) >= 0) {
+        links.splice(linkIndex, 1);
+      }
+      return null; //return null if not found
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
 });
 
